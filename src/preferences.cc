9ee96e855196f68b1259601daf09127005b3fee0
Mike O'Malley <spuriousdata@gmail.com>
2011-08-27 20:21:18 -0400

    changed old-style cast to static_cast

--- src/preferences.cc
+++ src/preferences.cc
@@ -16,7 +16,7 @@ Preferences::Preferences(QWidget *parent) :
 	parent_font = parent->font();
 
 	ui->setupUi(this);
-	ui->bytes_per_column_spinbox->setValue(((HexWidget*)parent)->get_bytes_per_column());
+	ui->bytes_per_column_spinbox->setValue(static_cast<HexWidget*>(parent)->get_bytes_per_column());
 	ui->font_button->setText(font_string(parent_font));
 
 	connect(ui->font_button, SIGNAL(clicked()), this, SLOT(show_font_dialog()));
